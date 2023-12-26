f9199ecda7d14c17f646a62f9a984b060ecb35ea
ShaoLung <jack82510@gmail.com>
2023-12-26 10:56:06 +0800

    hm1 upload

--- Test000/Program.cs
+++ Test000/Program.cs
@@ -4,12 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test000
+namespace jack
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            double x, y, z;
+            Console.WriteLine("請輸入x值");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("請輸入y值");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+            if (x > y)
+            {
+                z = x;
+                Console.WriteLine($"\"x比較大\",{z}");
+            }
+            else
+            {
+                z = y;
+                Console.WriteLine($"\"y比較大\",{z}");
+            }
+
+            Console.ReadKey();
         }
+
+
     }
 }
+
+
+
